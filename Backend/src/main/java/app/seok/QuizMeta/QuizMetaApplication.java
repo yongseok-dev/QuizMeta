000e96f8ce5aac14f341d1e7bb9c4a8eb1830b7b
Yongseok <yongseok.dev@gmail.com>
2023-04-20 14:42:25 +0900

    init: backend

---
+++ Backend/src/main/java/app/seok/QuizMeta/QuizMetaApplication.java
@@ -0,0 +1,12 @@
+package app.seok.QuizMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMetaApplication.class, args);
+	}
+}
