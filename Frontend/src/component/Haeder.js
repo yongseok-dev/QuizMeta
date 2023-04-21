d0b6b0028c59c5a1b0347d429cb0fd16eaa2af5f
Yongseok <yongseok.dev@gmail.com>
2023-04-21 09:30:03 +0900

    feat: header 초안

---
+++ Frontend/src/component/Haeder.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { MENU_DATA } from "../data/common";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            QuizMeta
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {MENU_DATA.map((e) => (
+                <li className="nav-item">
+                  <a
+                    className="nav-link active"
+                    aria-current="page"
+                    href={e.path}
+                  >
+                    {e.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ Frontend/src/data/common.js
@@ -0,0 +1,9 @@
+const MENU_DATA = [
+  { name: "Quiz목록", path: "/problems/category" },
+  { name: "Quiz등록", path: "/problems/new" },
+  { name: "오답노트", path: "/notes" },
+  { name: "Quiz복습", path: "/reviews/category" },
+  { name: "복습설정", path: "/reviews/setting" },
+];
+
+export { MENU_DATA };
